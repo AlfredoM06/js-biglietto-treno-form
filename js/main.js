9946c8a783f3017c13ab761c588ce6b857e5ae54
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-14 16:17:40 +0100

    started exercise

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let nameSurname = document.getElementById("user_name_surname").value;
+let km = document.getElementById("user_km").value;
+
+let ticketPrice = km * 0.21; 
+let ticketPriceforMinors = ticketPrice * 20 / 100;
+let ticketPriceforElders = ticketPrice * 40 / 100;
+
+if () {
+    ticketMinors = ticketPrice - ticketPriceforMinors;
+} else if () {
+    ticketPrice;
+} else () {
+    ticketElders = ticketPrice - ticketPriceforElders;
+}
