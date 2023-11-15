36e964e1a59a068d80a3ec22b8851e3f451081be
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-15 01:49:35 +0100

    exercise done without layout

--- js/main.js
+++ js/main.js
@@ -1,14 +1,28 @@
-let nameSurname = document.getElementById("user_name_surname").value;
-let km = document.getElementById("user_km").value;
+function calcTicket(event) {
+    event.preventDefault();
+    let nameSurname = document.getElementById("user_name_surname").value;
+    let km = document.getElementById("user_km").value;
 
-let ticketPrice = km * 0.21; 
-let ticketPriceforMinors = ticketPrice * 20 / 100;
-let ticketPriceforElders = ticketPrice * 40 / 100;
+    let ticketPrice = km * 0.21; 
+    let ticketPriceforMinors = ticketPrice * 20 / 100;
+    let ticketPriceforElders = ticketPrice * 40 / 100;
 
-if () {
-    ticketMinors = ticketPrice - ticketPriceforMinors;
-} else if () {
-    ticketPrice;
-} else () {
-    ticketElders = ticketPrice - ticketPriceforElders;
+    let selectAge = document.getElementById("select_age").value;
+
+
+    if (selectAge == 1) {
+        ticketPrice = ticketPrice - ticketPriceforMinors;
+    } else if (selectAge == 2) {
+        ticketPrice;
+    } else if (selectAge == 3) {
+        ticketPrice = ticketPrice - ticketPriceforElders;
+    }
+
+    let finalPrice = ticketPrice.toFixed(2);
+
+    document.getElementById("ticket_price").innerHTML = nameSurname + " " + finalPrice;
+
 }
+
+btn.addEventListener("click",calcTicket);
+
